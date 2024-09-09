3a8cd820ae099fbb7ccb107e4b26b11bd8f8ee16
Kurt1-cyber <delutakurtlouie@gmail.com>
2024-09-09 12:05:12 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+let firstFavNum = 41;
+let secondFavNum = 14;
+
+console.log("Addition:", firstFavNum + secondFavNum);
+console.log("Subtraction:", firstFavNum - secondFavNum);
+console.log("Multiplication:", firstFavNum * secondFavNum);
+console.log("Division:", firstFavNum / secondFavNum);
+consolo.log("Modulos:", firstFavNum % secondFavNum);
+console.log("Exponentiation (Power):", firstFavNum ** secondFavNum);
